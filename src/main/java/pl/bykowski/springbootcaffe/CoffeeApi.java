f63dbdf66cbe4181079bbf65101d016c9d89eadc
marcin <uhgugcv@op.pl>
2019-06-22 14:39:02 +0200

    New Commit

--- src/main/java/pl/bykowski/springbootcaffe/CoffeeApi.java
+++ src/main/java/pl/bykowski/springbootcaffe/CoffeeApi.java
@@ -1,86 +1,52 @@
 package pl.bykowski.springbootcaffe;
 
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api")
 public class CoffeeApi {
-
-    private List<Coffee> coffeList;
-
-    public CoffeeApi() {
-        coffeList = new ArrayList<>();
+    private CoffeeRepo coffeeRepo;
+    @Autowired
+    public CoffeeApi(CoffeeRepo coffeeRepo) {
+        this.coffeeRepo = coffeeRepo;
     }
 
     @GetMapping
-    public List<Coffee> getCoffes() {
-        return coffeList;
+    public Iterable<Coffee> getCoffes() {
+        return coffeeRepo.findAll();
     }
 
     @PostMapping
-    public void addCoffee(@RequestBody Coffee coffee) {
-        coffeList.add(coffee);
+    public void addCoffee(@RequestBody Coffee coffee)
+    {
+        coffeeRepo.save(coffee);
     }
 
     @DeleteMapping
-    public boolean removeCoffe(@RequestParam Long id) {
-        return coffeList.removeIf(element -> element.getId().equals(id));
-
+    public void removeCoffe(@RequestParam Long id) {
+        coffeeRepo.deleteById(id);
     }
 
     @PutMapping
-    public void putCoffee(@RequestBody Coffee coffee) {
-//        Optional<Coffee> first = coffeList.stream()
-//                .filter(element -> element.getId().equals(coffee.getId()))
-//                .findFirst();
-//        if (first.isPresent()) {
-//            coffeList.set(coffeList.indexOf(first.get()), coffee);
-//        }
-
-        Coffee foundElement = null;
-        for (Coffee coffee1 : coffeList) {
-            if(coffee1.getId().equals(coffee.getId()))
-            {
-                foundElement = coffee1;
-            }
-        }
-        coffeList.set(coffeList.indexOf(foundElement), coffee);
-    }
-
-
-    @GetMapping("/hello/{lang}")
-    public String get(@PathVariable String lang) {
-        if(lang.equals("PL"))
+    public void putCoffee(@RequestBody Coffee coffee)
+    {
+        Optional<Coffee> element = coffeeRepo.findById(coffee.getId());
+        if(element.isPresent())
         {
-            return "Cześć";
+            coffeeRepo.save(coffee);
         }
-        if(lang.equals("EN"))
-        {
-            return "Hello";
-        }
-        return "Niepojemaju";
     }
 
-
-
-
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void get() {
-        Coffee coffee1 = new Coffee(1L, "Prima", "Black");
-        Coffee coffee2 = new Coffee(2L, "Czibo", "Black");
-        Coffee coffee3 = new Coffee(3L, "Jacobs", "Black");
-        coffeList.add(coffee1);
-        coffeList.add(coffee2);
-        coffeList.add(coffee3);
-    }
-    public String my() {
-        return "Moja zamiana";
-    }
-
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void get() {
+//        Coffee coffee1 = new Coffee("Prima", "Black");
+//        Coffee coffee2 = new Coffee("Czibo", "Black");
+//        Coffee coffee3 = new Coffee("Jacobs", "Black");
+//        coffeeRepo.save(coffee1);
+//        coffeeRepo.save(coffee2);
+//        coffeeRepo.save(coffee3);
+//    }
 }
